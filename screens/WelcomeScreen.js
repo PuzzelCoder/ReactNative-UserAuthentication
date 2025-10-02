9343f7c8847e242945bcc0fe7cd08867ab7a0c1a
PuzzelCoder <79750251+PuzzelCoder@users.noreply.github.com>
2025-10-02 13:24:13 +0530

    Authentication using Firebase Authentication

---
+++ screens/WelcomeScreen.js
@@ -0,0 +1,39 @@
+import axios from "axios";
+import { useContext, useEffect, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { AuthContext } from "../util/auth-context";
+
+function WelcomeScreen() {
+  const [fetchedMessage, setFetchedMessage] = useState("");
+  const authCtx = useContext(AuthContext);
+  const token = authCtx.token;
+  const FIREBASE_AUTH = "FIREBASE_AUTH_LINK";
+  useEffect(() => {
+    axios.get(FIREBASE_AUTH + token).then((response) => {
+      setFetchedMessage(response.data);
+    });
+  }, []);
+  return (
+    <View style={styles.rootContainer}>
+      <Text style={styles.title}>Welcome!</Text>
+      <Text>You authenticated successfully!</Text>
+      <Text>{fetchedMessage}</Text>
+    </View>
+  );
+}
+
+export default WelcomeScreen;
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 32,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 8,
+  },
+});
